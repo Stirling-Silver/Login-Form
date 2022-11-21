7821a06dd03d8e35c1e86f9b201bc6a69cc59c7b
Stirling-Silver <113132128+Stirling-Silver@users.noreply.github.com>
2022-11-21 11:22:05 -0700

    Just the start. Got the valid system working on alerts

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 // JavaScript Code
+document.getElementById(`Username`);
+document.getElementById(`Password`);
+document.addEventListener("click", Login);
+
+//Login Processes
+let U = prompt(`Enter your username:`);
+
+let P = prompt(`Enter your password:`);
+
+if (U === `Admin` && P === `Key`) {
+  alert(`Valid!`);
+} else {
+  alert(`Invalid!`);
+}
